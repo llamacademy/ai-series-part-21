afa51f1d467657b3cdce8e9437b1b95c15930fcb
Chris Kurhan <support@llama.software>
2021-06-28 07:10:29 -0500

    Initial Commit

---
+++ Assets/Scripts/Enemy/Skills/SkillScriptableObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SkillScriptableObject : ScriptableObject
+{
+    public float Cooldown = 10f;
+    public int Damage = 5;
+    public int UnlockLevel = 1;
+
+    public bool IsActivating;
+
+    protected float UseTime;
+
+    public virtual SkillScriptableObject ScaleUpForLevel(ScalingScriptableObject Scaling, int Level)
+    {
+        SkillScriptableObject scaledSkill = CreateInstance<SkillScriptableObject>();
+
+        ScaleUpBaseValuesForLevel(scaledSkill, Scaling, Level);
+
+        return scaledSkill;
+    }
+
+    protected virtual void ScaleUpBaseValuesForLevel(SkillScriptableObject Instance, ScalingScriptableObject Scaling, int Level)
+    {
+        Instance.name = name;
+
+        Instance.Cooldown = Cooldown;
+        Instance.Damage = Damage + Mathf.FloorToInt(Scaling.DamageCurve.Evaluate(Level));
+        Instance.UnlockLevel = UnlockLevel;
+    }
+
+    public virtual void UseSkill(Enemy Enemy, Player Player)
+    {
+        IsActivating = true;
+    }
+
+    public virtual bool CanUseSkill(Enemy Enemy, Player Player, int Level)
+    {
+        return Level >= UnlockLevel;
+    }
+}
